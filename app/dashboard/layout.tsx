d3b6bf7613aa6f080270afe8a84e1a2186574462
Mahabub Hossain <palashmahabub@gmail.com>
2025-03-23 12:04:49 +0600

    add bike form

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,10 +11,10 @@ export default function AdminDashboardLayout({
       <Navbar />
       <div className="container mx-auto px-4 sm:px-0">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 py-5 md:py-10">
-          <div className="lg:col-span-8 2xl:col-span-9 order-1 lg:order-2">
+          <div className="lg:col-span-9 2xl:col-span-9 order-1 lg:order-2">
             {children}
           </div>
-          <div className="lg:col-span-4 2xl:col-span-3 order-2 lg:order-1 mb-6 lg:mb-0">
+          <div className="lg:col-span-3 2xl:col-span-3 order-2 lg:order-1 mb-6 lg:mb-0">
             <SideMenu />
           </div>
         </div>
--- components/dashboard/bike-information/bike-list.tsx
+++ components/dashboard/bike-information/bike-list.tsx
@@ -120,7 +120,7 @@ export default function BikeList({ bikedatas }: BikeProps) {
         {/* Engine Number Search */}
         <Input
           type="text"
-          placeholder="Search by Engine Number"
+          placeholder="Engine Number"
           value={engineNumber}
           onChange={(e) => setEngineNumber(e.target.value)}
         />
@@ -128,7 +128,7 @@ export default function BikeList({ bikedatas }: BikeProps) {
         {/* Chassis Number Search */}
         <Input
           type="text"
-          placeholder="Search by Chassis Number"
+          placeholder="Chassis Number"
           value={chassisNumber}
           onChange={(e) => setChassisNumber(e.target.value)}
         />
